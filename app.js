766b11e877b9f6730fa0df3a031b9b6d01645efc
NathanaelKhor <nwxkhor@gmail.com>
2025-02-05 12:11:35 +1100

    Some scroll animations added, fixed hamburger nav animation as well

---
+++ app.js
@@ -0,0 +1,13 @@
+const obsever = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => obsever.observe(el));
